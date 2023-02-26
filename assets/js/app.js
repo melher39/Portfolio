8618ca8bf0719870822d9c5a789aff76a54806e5
Melvin Hernandez <melher39@gmail.com>
2023-02-26 15:24:12 -0600

    small update

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,10 +2,9 @@
 $(function(){
 
 // starting point (index) of the text I want to type on the screen
-// let because it will change
 let i = 0;
 // text to be typed out
-const text = "Full Stack Web Developer";
+const text = "Software Engineer";
 // The speed/duration of the effect in milliseconds
 const speed = 80;
 
